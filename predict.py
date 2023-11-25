590efb5f773ab2eca6154f5e4a8f1ff72f682b64
Byron Mallett <byronated@gmail.com>
2023-11-26 01:38:42 +1300

    Added Stable Video Diffusion workflow and initial panorama support

--- predict.py
+++ predict.py
@@ -9,10 +9,35 @@ import shutil
 import uuid
 import json
 import urllib
+import io
 import websocket
 from PIL import Image
+import requests
 from urllib.error import URLError
 import random
+import numpy as np
+from equilib import equi2pers
+
+
+def upload_image(url, image: Image, image_name: str):
+    #image_format = get_image_format(image_path)
+
+    #if not image_format:
+    #    print(f"Error: Unsupported image format for {image_path}")
+    #    return
+
+    # Image needs to be file-like before upload
+    in_mem_file = io.BytesIO()
+    in_mem_file.name = image_name
+    image.save(in_mem_file, format=os.path.splitext(image_name)[1][1:])
+    in_mem_file.seek(0)
+    files = {'image': in_mem_file}
+
+    # Send the POST request using the requests library
+    response = requests.post(url, files=files)
+
+    # Print the server's response
+    print(f"Received response: {response}")
 
 
 class Predictor(BasePredictor):
@@ -50,11 +75,12 @@ class Predictor(BasePredictor):
         return json.loads(urllib.request.urlopen(req).read())
 
     def get_image(self, filename, subfolder, folder_type):
-        data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
-        print(folder_type)
-        url_values = urllib.parse.urlencode(data)
-        with urllib.request.urlopen("http://{}/view?{}".format(self.server_address, url_values)) as response:
-            return response.read()
+        params = {"filename": filename, "subfolder": subfolder, "type": folder_type}
+        response = requests.get(f"http://{self.server_address}/view", params=params, stream=True)
+        return response.raw
+
+        #with urllib.request.urlopen("http://{}/view?{}".format(self.server_address, url_values)) as response:
+            #return response.read()
 
     def get_images(self, ws, prompt, client_id):
         prompt_id = self.queue_prompt(prompt, client_id)['prompt_id']
@@ -76,11 +102,11 @@ class Predictor(BasePredictor):
                 node_output = history['outputs'][node_id]
                 print("node output: ", node_output)
 
-                if 'images' in node_output:
-                    images_output = []
-                    for image in node_output['images']:
+                images_output = []
+                if 'gifs' in node_output:
+                    for image in node_output['gifs']:
                         image_data = self.get_image(image['filename'], image['subfolder'], image['type'])
-                        images_output.append(image_data)
+                        images_output.append((image['filename'], image_data))
                 output_images[node_id] = images_output
 
         return output_images
@@ -92,13 +118,52 @@ class Predictor(BasePredictor):
     # TODO: add dynamic fields based on the workflow selected
     def predict(
         self,
-        input_prompt: str = Input(description="Prompt", default="beautiful scenery nature glass bottle landscape, purple galaxy bottle"),
-        negative_prompt: str = Input(description="Negative Prompt", default="text, watermark, ugly, blurry"),
+        image: Path = Input(
+            description="Inital image",
+        ),
+        azimuth_divisions: int = Input(
+            description="Number of horizontal divisions to split the panorama into",
+            default=8
+        ),
+        azimuth_width: int = Input(
+            description="Width of each starting image",
+            default=1024
+        ),
+        altitude_divisions: int = Input(
+            description="Number of vertical divisions to split the panorama into",
+            default=1
+        ),
+        altitude_height: int = Input(
+            description="Height of each starting image",
+            default=576
+        ),
         steps: int = Input(
             description="Steps",
-            default=30
+            default=20
         ),
-        seed: int = Input(description="Sampling seed, leave Empty for Random", default=None),
+        seed: int = Input(
+            description="Sampling seed, leave Empty for Random", 
+            default=None),
+        width: int = Input(
+            description="Output image width",
+            default=1024
+        ),
+        height: int = Input(
+            description="Output image height",
+            default=576
+        ),
+        video_frames: int = Input(
+            description="Number of frames to generate",
+            default=25
+        ),
+        motion_bucket_id: int = Input(
+            description="Higher values add more movement to the output. Values higher than 200 will hallucinate",
+            default=127
+        ),
+        fps: int = Input(
+            description="Framerate of the output",
+            default=6
+        )
     ) -> Path:
         """Run a single prediction on the model"""
         if seed is None:
@@ -106,20 +171,72 @@ class Predictor(BasePredictor):
         print(f"Using seed: {seed}")
         generator = torch.Generator("cuda").manual_seed(seed)
 
-        # queue prompt
-        img_output_path = self.get_workflow_output(
-            input_prompt = input_prompt,
-            negative_prompt = negative_prompt,
-            steps = steps,
-            seed = seed
-        )
-        return Path(img_output_path)
+        # Input equirectangular image
+        equi_img = Image.open(image)
+        equi_img = np.asarray(equi_img)
+        equi_img = np.transpose(equi_img, (2, 0, 1))
+
+        # Calculate how many iterations we will need to correctly calculate how many images we will need
+        outputs = []
+        azimuth_inc = (np.pi * 2) / azimuth_divisions
+        altitude_inc = (np.pi) / altitude_divisions
+
+        for azimuth_idx in range(azimuth_divisions):
+            for altitude_idx in range(altitude_divisions):
+
+                # rotations
+                rots = {
+                    'roll': 0.,
+                    'pitch': 0,  # rotate vertical
+                    'yaw': azimuth_inc * azimuth_idx,  # rotate horizontal
+                }
+
+                # Run equi2pers
+                pers_img = equi2pers(
+                    equi=equi_img,
+                    rots=rots,
+                    height=altitude_height,
+                    width=azimuth_width,
+                    fov_x=90.0,
+                    mode="bilinear",
+                )
+                pers_img = np.transpose(pers_img, (1, 2, 0))
+                pers_img = Image.fromarray(pers_img)
+
+                image_format = "png"
+
+                # queue prompt
+                img_output_path = self.get_workflow_output(
+                    init_image = pers_img,
+                    image_name = f"{image.stem}_{azimuth_idx}_{altitude_idx}.{image_format}",
+                    steps = steps,
+                    seed = seed,
+                    width = width,
+                    height = height,
+                    video_frames = video_frames,
+                    motion_bucket_id = motion_bucket_id,
+                    fps = fps
+                )
+                outputs.append(Path(img_output_path))
+
+        return outputs
 
 
-    def get_workflow_output(self, input_prompt, negative_prompt, steps, seed):
+    def get_workflow_output(
+        self, 
+        init_image: Image, 
+        image_name: str,
+        steps: int, 
+        seed: int,
+        width: int,
+        height: int,
+        video_frames: int,
+        motion_bucket_id: int,
+        fps: int
+    ):
         # load config
         prompt = None
-        workflow_config = "./custom_workflows/sdxl_txt2img.json"
+        workflow_config = "./custom_workflows/svd_api.json"
         with open(workflow_config, 'r') as file:
             prompt = json.load(file)
 
@@ -127,11 +244,19 @@ class Predictor(BasePredictor):
             raise Exception('no workflow config found')
 
         # set input variables
-        prompt["6"]["inputs"]["text"] = input_prompt
-        prompt["7"]["inputs"]["text"] = negative_prompt
-
         prompt["3"]["inputs"]["seed"] = seed
         prompt["3"]["inputs"]["steps"] = steps
+        prompt["12"]["inputs"]["width"] = width
+        prompt["12"]["inputs"]["height"] = height
+        prompt["12"]["inputs"]["video_frames"] = video_frames
+        prompt["12"]["inputs"]["motion_bucket_id"] = motion_bucket_id
+        prompt["12"]["inputs"]["fps"] = fps
+        prompt["23"]["inputs"]["image"] = image_name
+        prompt["26"]["inputs"]["frame_rate"] = fps
+        prompt["26"]["inputs"]["filename_prefix"] = image_name
+
+        # Upload init image
+        upload_image("http://{}/upload/image".format(self.server_address), init_image, image_name)
 
         # start the process
         client_id = str(uuid.uuid4())
@@ -140,9 +265,8 @@ class Predictor(BasePredictor):
         images = self.get_images(ws, prompt, client_id)
 
         for node_id in images:
-            for image_data in images[node_id]:
-                from PIL import Image
-                import io
-                image = Image.open(io.BytesIO(image_data))
-                image.save("out-"+node_id+".png")
-                return Path("out-"+node_id+".png")
+            for image_filename, image_data in images[node_id]:
+                print(f"About to write {image_filename}")
+                with open(image_filename, 'wb') as out_file:
+                    shutil.copyfileobj(image_data, out_file)
+                    return Path(image_filename)
